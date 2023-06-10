d2f58a5c0243fe1f7261f686bc128af5838341a4
leo <qianxunlan@qq.com>
2023-06-10 14:00:30 +0800

    add element-plus dialog component demo with provide inject

--- src/components/MyDialog.vue
+++ src/components/MyDialog.vue
@@ -1,5 +1,5 @@
 <template>
-    <el-dialog v-model="visible" title="用户录入">
+    <el-dialog v-model="dialogValue.visible" title="用户录入">
         <el-form>
             <el-form-item label="姓名">
                 <el-input v-model="name" autocomplete="off" />
@@ -10,34 +10,24 @@
         </el-form>
         <template #footer>
             <span class="dialog-footer">
-                <el-button @click="visible = false">取消</el-button>
+                <el-button @click="dialogValue.visible = false">取消</el-button>
                 <el-button type="primary" @click="handleClick">确认</el-button>
             </span>
         </template>
     </el-dialog>
 </template>
 
-<script setup lang="ts">
-import { computed, ref } from 'vue';
+<script setup>
+import { inject, ref } from 'vue';
+const dialogValue = inject('dialogValue')
 
-const props = defineProps(['visible', 'modelValue'])
-const emit = defineEmits(['update:visible', 'update:modelValue'])
-
-
-const visible = computed({
-    get() {
-        return props.visible
-    },
-    set(value) {
-        emit('update:visible', value)
-    }
-})
-
+// 不直接修改dialogValue.name等属性，确定时再提交
 const name = ref('')
 const email = ref('')
 
 const handleClick = () => {
-    emit('update:visible', false)
-    emit('update:modelValue', { name, email })
+    dialogValue.value.visible = false
+    dialogValue.value.name = name
+    dialogValue.value.email = email
 }
 </script>
